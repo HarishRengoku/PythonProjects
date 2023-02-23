e4d6f9a790a03722b661ddc61a5ee05c5828843c
Harish Ganesh <94498641+HarishRengoku@users.noreply.github.com>
2023-02-23 18:39:51 +0530

    Update Tip_calculator.py

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -6,6 +6,9 @@ tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 
 num_of_split = int(input("How many people to split the bill? "))
 
-total= (((tip/100)+1)*bill)/num_of_split
+tip_only= ((tip/100)*bill)/num_of_split
+eat = bill/num_of_split
 
-print(f"Each person needs to pay {round(total,2)}$ to cover the bill")
+total = eat + tip_only
+
+print(f"Each person needs to pay {round(total,2)}$ to cover the bill along with the tip. \nthe tip per person is {tip_only} and the bill per person is {eat}")
